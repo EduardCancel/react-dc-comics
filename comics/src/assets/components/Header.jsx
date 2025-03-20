92598fbbb4474c52d53db9ce4f05e94c57f34726
IlTuoNome <laTuaEmail@example.com>
2025-03-20 16:50:25 +0100

    feat: implement Header component with navigation and styles

--- comics/src/assets/components/Header.jsx
+++ comics/src/assets/components/Header.jsx
@@ -1,5 +1,24 @@
 export default function Header(){
     return(
-        <h1>Header</h1>
+        <header className="cotainer-header">
+            <div className="cotainer">
+                <img src="../public/img/dc-logo.png" alt="" />
+                <nav id="menu">
+                    <ul>
+                        <li><a href="">CHARACTERS</a></li>
+                        <li><a href="">COMICS</a></li>
+                        <li><a href="">MOVIES</a></li>
+                        <li><a href="">TV</a></li>
+                        <li><a href="">GAMES</a></li>
+                        <li><a href="">COLLECTIBLES</a></li>
+                        <li><a href="">VIDEOS</a></li>
+                        <li><a href="">FANS</a></li>
+                        <li><a href="">NEW</a></li>
+                        <li><a href="">SHOP</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        
     )
 }
