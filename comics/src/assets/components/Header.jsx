a59d78faf6a2180d0a75a2d8bdd1bb181fd74378
IlTuoNome <laTuaEmail@example.com>
2025-03-20 17:30:04 +0100

    fix: correct typo in container class name and enhance header layout styles

--- comics/src/assets/components/Header.jsx
+++ comics/src/assets/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return(
         <header className="cotainer-header">
-            <div className="cotainer">
+            <div className="container">
                 <img src="../public/img/dc-logo.png" alt="" />
                 <nav id="menu">
                     <ul>
