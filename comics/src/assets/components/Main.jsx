1839c28362a156074e6cf49d6464312bab6a5ba5
IlTuoNome <laTuaEmail@example.com>
2025-03-20 18:05:42 +0100

    feat: enhance Main component layout and styles with new sections and improved CSS

--- comics/src/assets/components/Main.jsx
+++ comics/src/assets/components/Main.jsx
@@ -1,7 +1,19 @@
 export default function Main(){
     return (
         <main>
-            <h1>Main</h1>
+            <section className="up">
+                <h3>
+                    Content Goes Here
+                </h3>
+            </section>
+            <section className="down">
+                <ul>
+                    <li>
+                        <img className="icon" src="../public/img/buy-comics-digital-comics.png" alt="" width={40} />
+                        <a href=""><span>DIGITAL COMICS</span></a>
+                    </li>
+                </ul>
+            </section>
         </main>
     )
     
