7daa11dafaff9f638cce93beacb8d68de44def18
IlTuoNome <laTuaEmail@example.com>
2025-03-21 01:16:10 +0100

    feat: redesign Footer and Main components with improved layout and styles

--- comics/src/assets/components/Main.jsx
+++ comics/src/assets/components/Main.jsx
@@ -1,4 +1,4 @@
-export default function Main(){
+export default function Main() {
     return (
         <main>
             <section className="up">
@@ -7,28 +7,28 @@ export default function Main(){
             <section className="down">
                 <ul>
                     <li>
-                        <img className="icon" src="../public/img/buy-comics-digital-comics.png" alt="" width={50} />
+                        <img className="icon" src="../public/img/buy-comics-digital-comics.png" alt="" width={35} />
                         <a href=""><span>DIGITAL COMICS</span></a>
                     </li>
                     <li>
-                        <img className="icon" src="../public/img/buy-comics-merchandise.png" alt="" width={50} />
+                        <img className="icon" src="../public/img/buy-comics-merchandise.png" alt="" width={35} />
                         <a href=""><span>DC MERCHANDISE</span></a>
                     </li>
                     <li>
-                        <img className="icon" src="../public/img/buy-comics-subscriptions.png" alt="" width={50} />
+                        <img className="icon" src="../public/img/buy-comics-subscriptions.png" alt="" width={35} />
                         <a href=""><span>SUBSCRIPTION</span></a>
                     </li>
                     <li>
-                        <img className="icon" src="../public/img/buy-comics-shop-locator.png" alt="" width={40} />
+                        <img className="icon" src="../public/img/buy-comics-shop-locator.png" alt="" width={25} />
                         <a href=""><span>COMIC SHOP LOCATOR</span></a>
                     </li>
                     <li>
-                        <img className="icon" src="../public/img/buy-dc-power-visa.svg" alt="" width={60} />
+                        <img className="icon" src="../public/img/buy-dc-power-visa.svg" alt="" width={40} />
                         <a href=""><span>DC POWER VISA</span></a>
                     </li>
                 </ul>
             </section>
         </main>
     )
-    
+
 }
