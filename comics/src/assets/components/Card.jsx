cbd1ab9bee7e325db0939254528a046fc51f0578
IlTuoNome <laTuaEmail@example.com>
2025-03-21 17:31:15 +0100

    feat: refactor Card and ComicList components for improved layout and styles; update CSS

--- comics/src/assets/components/Card.jsx
+++ comics/src/assets/components/Card.jsx
@@ -1,13 +1,9 @@
 export default function Card({ image, title }) {
 
     return (
-        <div className="container-cards">
-            <div className="rowCards">
-                <div className="col-2">
-                    <img src={image} alt={title} className="comic-card-image" />
-                    <p className="comic-card-title">{title.toUpperCase()}</p>
-                </div>
-            </div>
+        <div className="comic-card">
+            <img src={image} alt={title} className="card-image" />
+            <p className="card-title">{title.toUpperCase()}</p>
         </div>
     )
 }
