910ed8f65d616be49d2d5ee62d5761f8c68c2f58
IlTuoNome <laTuaEmail@example.com>
2025-03-23 11:55:11 +0100

    add structured footer with navigation links and social media buttons; update styles

--- comics/src/assets/components/Footer.jsx
+++ comics/src/assets/components/Footer.jsx
@@ -1,7 +1,66 @@
 export default function Footer() {
     return (
         <>
+            <section className="footer-middle">
+                <div>
+                    <ul>
+                        <li><h3>DC COMICS</h3></li>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                    <ul>
+                        <li><h3>SHOP</h3></li>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+                    <img className="logo-bg" src="/img/dc-logo-bg.png" alt="" srcset="" />
+                </div>
+                <div>
+                    <ul>
+                        <li><h3>DC</h3></li>
+                        <li>Terms Of Use</li>
+                        <li>Privacy Policy (New)</li>
+                        <li>Ad Choises</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscriptions</li>
+                        <li>Talent Workshops</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li><h3>SITES</h3></li>
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
+                    </ul>
+                </div>
+            </section>
+            <section className="footer-bottom">
+                <div className="btn-section">
+                    <button className="btn">SIGN-UP NOW!</button>
+                </div>
+                <div className="social-section">
+                    <h3>FOLLOW US</h3>
+                    <img src="/img/footer-facebook.png" alt="" srcset="" />
+                    <img src="/img/footer-twitter.png" alt="" srcset="" />
+                    <img src="/img/footer-youtube.png" alt="" srcset="" />
+                    <img src="/img/footer-pinterest.png" alt="" srcset="" />
+                    <img src="/img/footer-periscope.png" alt="" srcset="" />
+                </div>
 
+            </section>
         </>
     )
 }
